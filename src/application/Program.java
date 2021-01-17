d84fd50f92b1a4c1c32ca3ef3cf4f63f9e2a0161
mmelero <mmelero@gmail.com>
2021-01-17 18:07:55 -0300

    Departament entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.departament;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		departament obj = new departament(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
