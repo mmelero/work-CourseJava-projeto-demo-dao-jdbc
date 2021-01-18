a9061bdd162499fe60144a323a0ee3b9e314319e
mmelero <mmelero@gmail.com>
2021-01-18 17:58:58 -0300

    Delete Seller implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(3);
@@ -43,11 +45,21 @@ public class Program {
 		sellerDao.update(seller);
 		System.out.println("Updat complete!");
 		
-		System.out.println("\n=== TEST 3: seller findAll =====");
 		list = sellerDao.findAll();
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		System.out.println("\n=== TEST 6: seller Delete =====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete complete!");
+		
+		list = sellerDao.findAll();
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+
 	}
 
 }
