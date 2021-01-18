32ccde4d85f49e8fcd7525f9efe2441865a10f3e
mmelero <mmelero@gmail.com>
2021-01-18 17:41:23 -0300

    Update Seller implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,8 +36,18 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = "+ newSeller.getId());
-	
 
+		System.out.println("\n=== TEST 5: seller Update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Updat complete!");
+		
+		System.out.println("\n=== TEST 3: seller findAll =====");
+		list = sellerDao.findAll();
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
